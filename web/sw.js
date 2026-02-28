35c79e7c67e6f1df32ffde3fc9443ed62b28efa2
okalmary254 <okalmohn18@gmail.com>
2026-02-28 23:54:26 +0300

    added meta tags for pwa

--- web/sw.js
+++ web/sw.js
@@ -8,6 +8,7 @@ const STATIC_ASSETS = [
   '/styles.css',
   '/app.js',
   '/manifest.json',
+  'icons/icon-192.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-solid-900.woff2',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-brands-400.woff2'
